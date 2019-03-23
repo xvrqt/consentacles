818690bd0e3501c2d03d5d7793bf18ab7152fef8
Amy <git@xvrqt.com>
2019-03-23 09:31:37 -0700

    new project <name> now creates the correct directory structure

--- cli/help.js
+++ cli/help.js
@@ -14,11 +14,16 @@ Usage:
     ${chalk.dim('$ consentacles <command> (option)')}
 
 Commands:
-    - new
+    - new         ${chalk.dim('create a new <type> with <name>')}
     - help        ${chalk.dim('prints this text')}
     - build
     - serve
     - version     ${chalk.dim('prints the version number')}
+
+New:
+    - project
+        Usage: ${chalk.dim('$ consentacles new project foo')}
+        ${chalk.dim('This will create a new consentacles project space in a directory name \'foo\'')}
 `;
 
 module.exports = {
--- cli/new/new.js
+++ cli/new/new.js
@@ -7,6 +7,9 @@ if(process.env.chalk === 'disabled') {
 	chalk.enabled = false;
 }
 
+/* CLI input/output */
+// const inquirer = require('inquirer');
+
 /*******************
 * HELPER FUNCTIONS *
 ********************/
@@ -28,6 +31,19 @@ function printSubtle(text) {
 	console.log(chalk.dim(text));
 }
 
+/* Wraps mkdir in a promise to make it easier to handle */
+function mkdir(name, recursive) {
+	return new Promise((resolve, reject) => {
+		recursive = recursive ? recursive : false;
+		fs.mkdir(name, {recursive: recursive}, (error) => {
+			if(error) {
+				printError(error);
+				reject();
+			} else { resolve(); }
+		})
+	})
+}
+
 /* CONSTANTS */
 const types = [
 	"component",
@@ -36,22 +52,53 @@ const types = [
 	"page"
 ];
 
+/* Default Settings for each route */
+const defaults = {
+	project: {
+		name: "tentacle",
+		server: null,
+		dockerize: null
+	}
+};
+
+const project_directories = [
+	"dist",
+	"src/components",
+	"src/icons",
+	"src/images",
+	"src/meta",
+	"src/pages",
+	"src/scripts",
+	"src/styles"
+];
+
+/* Project Route */
+async function project(name) {
+	directory_created = [mkdir(name)];
+	project_directories.forEach((value) => {
+		directory_created.push(mkdir(`${name}/${value}`, true));
+	});
+	await Promise.all(directory_created);
+	console.log('done');
+}
+
 /* 'new' can create many things, this function hands off control to the correct
  * sub-module to make those things happen.
 */
 function dispatch(type, name) {
 	switch (type) {
 		case 'project':
-			console.log(type, name);
+			project(name);
 			break;
 		default:
 			printError('Unrecognized option provided.');
-			printSubtle(`You can use 'new' to create: ${commands.join('s, ')}`);
-			printSubtle(`Example: $ consentacles new project`);
+			printSubtle(`You can use 'new' to create ${types.map((str) => { return str + "s";}).join(', ')}`);
+			printSubtle(`Example: $ consentacles new project foo`);
 			process.exit(1);					
 	}
 }
 
 module.exports = {
-    run: dispatch
+    run: dispatch,
+    types: types
 };
